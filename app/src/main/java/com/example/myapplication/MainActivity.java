cacb3bfe1ac0a982382610007764517547d1a61b
K666666 <1401147044@qq.com>
2020-09-14 11:21:46 +0800

    第二次提交

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -11,6 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("ddd");
         Log.d(TAG, "onCreate: ");
+        Log.d(TAG, "onCreate: ");
+        Log.i(TAG, "onCreate: ");
+        Log.d(TAG, "onCreate() called with: savedInstanceState = [" + savedInstanceState + "]");
     }
 }
